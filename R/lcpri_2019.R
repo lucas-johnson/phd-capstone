b060fc1929db1afde1751c72940cfd6cd6720c2f
Lucas Johnson <lucas.k.johnson03@gmail.com>
2024-02-11 17:37:26 -0500

    colin's feedback

--- R/lcpri_2019.R
+++ R/lcpri_2019.R
@@ -116,3 +116,40 @@ ggsave(
   height = 190,
   units = "mm"
 )
+
+
+lcmap_w_plots20 <- ggplot() +
+  geom_raster(data = lcpri_data, aes(x, y, fill = LCPRI)) +
+  geom_sf(data = state_shoreline, color = 'black', fill = NA) +
+  geom_sf(data = fia_plots[sample(1:nrow(fia_plots), .2 * nrow(fia_plots)),], color = 'black', size = 0.6) +
+  scale_fill_manual(name = "", values = class_fill_scale, labels = class_colors$classes, na.value = 'transparent') +
+  theme_minimal() +
+  theme(
+    legend.position = "bottom",
+    legend.text = element_text(size = 12)
+  ) +
+  xlab(NULL) +
+  ylab(NULL) +
+  annotation_scale(
+    location = "bl",
+    text_cex = .8,
+    pad_x = unit(0, "cm")
+  ) +
+  annotation_north_arrow(
+    which_north = 'true',
+    location = "tr",
+    style = north_arrow_orienteering(
+      fill = c("black", "black")
+    ),
+    height = unit(0.3, "in"),
+    width = unit(0.3, "in")
+  )
+
+ggsave(
+  "figures/lcmap_x_plots20_2019.png",
+  lcmap_w_plots20,
+  dpi = 300,
+  width = 190,
+  height = 190,
+  units = "mm"
+)
